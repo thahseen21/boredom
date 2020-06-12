0f3e037eab4cba1c77c0dabbd40e74ff9b1933dd
thahseen21 <52756349+thahseen21@users.noreply.github.com>
2020-06-12 20:35:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+docelm=document.documentElement;
+body=document.body;
+window.addEventListener("scroll",function(event){
+  var nav=document.getElementById("nav");
+  scrollPos=body.scrollTop||docelm.scrollTop;
+  nav.className = (scrollPos > 20)? "navbar-scrolled":"navbar";
+});
+
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+window.addEventListener("mouseenter",function(event){
+  var x=document.getElementById("underline");
+  x.className="visible";
+  console.log("potato")
+});
